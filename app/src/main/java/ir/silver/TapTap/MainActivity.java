ffb2532cc6702933350e2709281bdf2bd75f532c
mobinghaemi <mobinghaemi@yahoo.com>
2023-06-18 11:58:59 +0330

    Edit Package Name, Add 2 Buttons, Better SplashScreen, Fix Bugs and ...

--- app/src/main/java/ir/silver/TapTap/MainActivity.java
+++ app/src/main/java/ir/silver/TapTap/MainActivity.java
@@ -2,6 +2,9 @@ package ir.silver.TapTap;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
+import ir.silver.TapTap.*;
 
 public class MainActivity extends Activity 
 {
@@ -11,6 +14,14 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-		
     }
+	public void btnHandler(View v)
+	{
+		Toast.makeText(this, "Im a toast message and love sushi 🥳", Toast.LENGTH_SHORT).show();
+	}
+	public void exitHandler(View v)
+	{
+		//Button exitBtn = (Button) findViewById(R.id.exitBtn);
+		finish();
+	}
 }
